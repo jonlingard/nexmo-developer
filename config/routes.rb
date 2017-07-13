7133c0f20b2b9bae8823a8bdf4f78fd69e055f98
Adam Butler <adam@lab.io>
2017-07-13 15:11:23 +0100

    Copy tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,6 @@ Rails.application.routes.draw do
   get '/community/past-events', to: 'static#past_events'
   get '/changelog', to: 'static#changelog'
 
-
   match '/search', to: 'search#results', via: [:get, :post]
   match '/quicksearch', to: 'search#quicksearch', via: [:get, :post]
 
